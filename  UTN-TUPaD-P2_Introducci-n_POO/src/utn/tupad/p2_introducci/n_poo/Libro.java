d694ef9071959badb8c5bfd937f7534a9507813b
donramones <agusrivarolaa00@gmail.com>
2025-09-15 23:29:28 -0300

    10ok

---  UTN-TUPaD-P2_Introducci-n_POO/src/utn/tupad/p2_introducci/n_poo/Libro.java
+++  UTN-TUPaD-P2_Introducci-n_POO/src/utn/tupad/p2_introducci/n_poo/Libro.java
@@ -9,5 +9,26 @@ package utn.tupad.p2_introducci.n_poo;
  * @author rivar
  */
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int añoPublicacion;
     
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    public String getAutor (){
+        return autor;
+    }
+    
+    public int getAñoPublicacion(){
+        return añoPublicacion;
+    }
+    public void setAñoPublicacion(int añoPublicacion){
+        if (añoPublicacion != 0){
+            this.añoPublicacion = añoPublicacion;
+        }else{
+            System.out.println("Anio invalido");
+        }
+    }
 }
